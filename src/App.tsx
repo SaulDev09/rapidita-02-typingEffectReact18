ddfb5828fd5b8704c1f268b14c99357b2e16270c
Saul Chipana <Saul.Dev09@gmail.com>
2023-06-04 23:01:09 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './style.scss'
+
+function App() {
+
+  return (
+    <>
+      <div className="input-wrapper">
+        <input aria-label="Ask us anything" />
+        <span className="placeholder"></span>
+      </div>
+      <section>
+        <img src='images/bg.jpg' id="bg" />
+      </section>
+
+    </>
+  )
+}
+
+export default App
